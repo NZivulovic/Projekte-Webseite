8e808548c561677a5ca77afd4f197dbb925313d2
Nikola Zivulovic <104269774+NZivulovic@users.noreply.github.com>
2025-11-29 15:24:08 +0100

    Mehr Infos button functionality + misc

--- main.js
+++ main.js
@@ -3,3 +3,19 @@ const moreNav_1 = document.getElementById("myDropdown")
 function moreNav(){
     document.getElementById("myDropdown").classList.toggle("show");
 }
+
+window.onclick = function(event){
+    if (!event.target.matches(".dropdownButtonNav")){
+        var dropdowns = document.getElementsByClassName("dropdownContent");
+        var i;
+        for (i = 0; i < dropdowns.length; i++){
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+
