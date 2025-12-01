656ea5082c18128e55045eb81f6ced3b3daa05ce
Nikola Zivulovic <104269774+NZivulovic@users.noreply.github.com>
2025-12-01 15:31:26 +0100

    moved index.html

--- js/godot.js
+++ js/godot.js
@@ -1,1 +1,17 @@
 
+// Projekt Selector navBar
+
+function navBarSelector(targetId) {
+  
+  document.querySelectorAll('.projektContainer').forEach(el => {
+    el.style.display = 'none';
+  });
+  
+  
+  const targetElement = document.getElementById(targetId);
+
+  
+  if (targetElement) {
+    targetElement.style.display = 'block';
+  }
+}
