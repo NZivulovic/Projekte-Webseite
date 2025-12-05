6e4e81d671c101ec18a325c686dff70397419196
Nikola Zivulovic <104269774+NZivulovic@users.noreply.github.com>
2025-12-05 12:52:22 +0100

    css + html rearranged

--- js/godot.js
+++ js/godot.js
@@ -1,20 +1,16 @@
-
 // Projekt Selector navBar
 //--------------------------------------------------//
 function navBarSelector(targetId) {
-
-  document.querySelectorAll('.projektContainer').forEach(el => {
-    el.style.display = 'none';
+  document.querySelectorAll(".projektContainer").forEach((el) => {
+    el.style.display = "none";
   });
-
 
   const targetElement = document.getElementById(targetId);
 
-
   if (targetElement) {
-    targetElement.style.display = 'block';
+    targetElement.style.display = "block";
   }
-};
+}
 //--------------------------------------------------//
 
 // Complete function to cycle images from imgGallery and reset Button //
@@ -24,65 +20,55 @@ function navBarSelector(targetId) {
   let newestImg;
 
   function switchPreview(targetId) {
-
     let vid = document.getElementById("videoProjekt1");
     let img = document.getElementById(`${targetId}`);
 
     if (vid != null) {
-
-      vid.replaceWith(newImg = new (Image));
+      vid.replaceWith((newImg = new Image()));
       newImg.src = `${img.src}`;
 
-
-      newImg.style.display = 'block';
-      newImg.style.marginLeft = 'auto';
-      newImg.style.marginRight = 'auto';
-      newImg.style.width = '50%';
-      newImg.style.border = '2px solid rgb(102, 115, 138)';
-
-
+      newImg.style.display = "block";
+      newImg.style.marginLeft = "auto";
+      newImg.style.marginRight = "auto";
+      newImg.style.width = "50%";
+      newImg.style.border = "2px solid rgb(102, 115, 138)";
     }
 
     if (vid == null) {
-      newImg.replaceWith(newestImg = newImg);
+      newImg.replaceWith((newestImg = newImg));
       newestImg.src = `${img.src}`;
 
-
-      newestImg.style.display = 'block';
-      newestImg.style.marginLeft = 'auto';
-      newestImg.style.marginRight = 'auto';
-      newestImg.style.width = '50%';
-      newestImg.style.border = '2px solid rgb(102, 115, 138)';
-
+      newestImg.style.display = "block";
+      newestImg.style.marginLeft = "auto";
+      newestImg.style.marginRight = "auto";
+      newestImg.style.width = "50%";
+      newestImg.style.border = "2px solid rgb(102, 115, 138)";
     }
 
     if (newVid != null) {
-      newVid.replaceWith(newestImg = new (Image));
+      newVid.replaceWith((newestImg = new Image()));
       newestImg.src = `${img.src}`;
 
+      newestImg.style.display = "block";
+      newestImg.style.marginLeft = "auto";
+      newestImg.style.marginRight = "auto";
+      newestImg.style.width = "50%";
+      newestImg.style.border = "2px solid rgb(102, 115, 138)";
 
-      newestImg.style.display = 'block';
-      newestImg.style.marginLeft = 'auto';
-      newestImg.style.marginRight = 'auto';
-      newestImg.style.width = '50%';
-      newestImg.style.border = '2px solid rgb(102, 115, 138)';
-
-      newVid.id = vid.id
+      newVid.id = vid.id;
     }
   }
 
   let newVid = document.createElement("video");
-  const vidSource = "../videos/Timeline 1.mp4";
+  const vidSource = "../videos/godotProjekte/projekt1/Timeline 1.mp4";
 
   function resetVideo() {
-
     newVid.src = vidSource;
 
     newImg.replaceWith(newVid);
     newestImg.replaceWith(newVid);
 
-    newVid.controls = "options"
-
+    newVid.controls = "options";
 
     newVid.style.display = "flex";
     newVid.style.flexWrap = "wrap";
@@ -91,16 +77,13 @@ function navBarSelector(targetId) {
     newVid.style.minWidth = "400px";
     newVid.style.minHeight = "200px";
 
-
     newVid.style.marginLeft = "auto";
     newVid.style.marginRight = "auto";
     newVid.style.width = "50%";
 
     newVid.style.border = "2px solid rgb(102, 115, 138)";
-
-
   }
-};
+}
 //--------------------------------------------------//
 
 // Scroll with MouseWheel - imgGallery Horizontal
@@ -111,12 +94,10 @@ function navBarSelector(targetId) {
     if (e.deltaY > 0) {
       container.scrollLeft += 200;
       e.preventDefault();
-    }
-    else {
+    } else {
       container.scrollLeft -= 200;
       e.preventDefault();
     }
   });
-};
+}
 //--------------------------------------------------//
-
--- js/unity.js
+++ js/unity.js
@@ -1,16 +1,103 @@
 // Projekt Selector navBar
-
+//--------------------------------------------------//
 function navBarSelector(targetId) {
-
-  document.querySelectorAll('.projektContainer').forEach(el => {
-    el.style.display = 'none';
+  document.querySelectorAll(".projektContainer").forEach((el) => {
+    el.style.display = "none";
   });
-
 
   const targetElement = document.getElementById(targetId);
 
-
   if (targetElement) {
-    targetElement.style.display = 'block';
+    targetElement.style.display = "block";
   }
 }
+//--------------------------------------------------//
+
+// Complete function to cycle images from imgGallery and reset Button //
+//--------------------------------------------------//
+{
+  let newImg;
+  let newestImg;
+
+  function switchPreview(targetId) {
+    let vid = document.getElementById("videoProjekt1");
+    let img = document.getElementById(`${targetId}`);
+
+    if (vid != null) {
+      vid.replaceWith((newImg = new Image()));
+      newImg.src = `${img.src}`;
+
+      newImg.style.display = "block";
+      newImg.style.marginLeft = "auto";
+      newImg.style.marginRight = "auto";
+      newImg.style.width = "50%";
+      newImg.style.border = "2px solid rgb(168, 187, 219)";
+    }
+
+    if (vid == null) {
+      newImg.replaceWith((newestImg = newImg));
+      newestImg.src = `${img.src}`;
+
+      newestImg.style.display = "block";
+      newestImg.style.marginLeft = "auto";
+      newestImg.style.marginRight = "auto";
+      newestImg.style.width = "50%";
+      newestImg.style.border = "2px solid rgb(168, 187, 219)";
+    }
+
+    if (newVid != null) {
+      newVid.replaceWith((newestImg = new Image()));
+      newestImg.src = `${img.src}`;
+
+      newestImg.style.display = "block";
+      newestImg.style.marginLeft = "auto";
+      newestImg.style.marginRight = "auto";
+      newestImg.style.width = "50%";
+      newestImg.style.border = "2px solid rgb(168, 187, 219)";
+
+      newVid.id = vid.id;
+    }
+  }
+
+  let newVid = document.createElement("video");
+  const vidSource = "../videos/unityProjekte/projekt1/Timeline 2.mp4";
+
+  function resetVideo() {
+    newVid.src = vidSource;
+
+    newImg.replaceWith(newVid);
+    newestImg.replaceWith(newVid);
+
+    newVid.controls = "options";
+
+    newVid.style.display = "flex";
+    newVid.style.flexWrap = "wrap";
+    newVid.style.justifyContent = " baseline";
+
+    newVid.style.minWidth = "400px";
+    newVid.style.minHeight = "200px";
+
+    newVid.style.marginLeft = "auto";
+    newVid.style.marginRight = "auto";
+    newVid.style.width = "50%";
+
+    newVid.style.border = "2px solid rgb(168, 187, 219)";
+  }
+}
+//--------------------------------------------------//
+
+// Scroll with MouseWheel - imgGallery Horizontal
+//--------------------------------------------------//
+{
+  const container = document.getElementById("galleryItem");
+  container.addEventListener("wheel", function (e) {
+    if (e.deltaY > 0) {
+      container.scrollLeft += 200;
+      e.preventDefault();
+    } else {
+      container.scrollLeft -= 200;
+      e.preventDefault();
+    }
+  });
+}
+//--------------------------------------------------//
